fa8882adb83c4b82f07771d9bdfa39ff247f9996
Miguel <miguelb1995@icloud.com>
2018-04-29 02:42:08 -0500

    Cambio nombre tablas a memoria

--- Analysis/src/VirtualMachine.py
+++ Analysis/src/VirtualMachine.py
@@ -1,18 +1,8 @@
 from cuadruplos import *
 from parser import *
 from structs import * 
+from tablas import *
 import sys
 
-MemoryInt = {}
 
 
-def iniciaVM():
-    main()
-
-
-def main()
-    while cuadruplos.count != 0
-        for cuad in cuadruplos
-            if cuad.op == '+':
-                resu = cuad.opdoDer + cuad.opdoIzq
-                MemoryInt = {"Memoria":cuad.res,"Value": resu}
